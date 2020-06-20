0cc5642667cdb6a0fa0e9fd8499c791ab03940fa
hcl <2635672750@qq.com>
2020-06-20 10:42:57 +0800

    第二次提交

---
+++ seata-order-service2002/src/main/java/com/atguigu/springcloud/alibaba/service/impl/StorageServiceImpl.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.alibaba.service.impl;
+
+import com.atguigu.springcloud.alibaba.dao.StorageDao;
+import com.atguigu.springcloud.alibaba.service.StorageService ;
+import io.seata.core.context.RootContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+
+@Service
+public class StorageServiceImpl implements StorageService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StorageServiceImpl.class);
+
+    @Resource
+    private StorageDao storageDao;
+
+    // 扣减库存
+    @Override
+    public void decrease(Long productId, Integer count) {
+        LOGGER.info("------->storage-service中扣减库存开始");
+        System.out.println(RootContext.getXID()+"——————————————————————————————————");
+//        try { TimeUnit.SECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
+        storageDao.decrease(productId,count);
+        LOGGER.info("------->storage-service中扣减库存结束");
+    }
+}
+
